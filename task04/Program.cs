bae5af1fb8171b531571f72874ea84da5272e795
TatNik Mosku <lusmo1@yandex.ru>
2023-03-11 10:32:34 +0700

    Программа task04

---
+++ task04/Program.cs
@@ -0,0 +1,24 @@
+﻿// 
+// 
+// 
+// 
+// 
+
+Console.WriteLine("Введите первое число");
+int a=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int c3=Convert.ToInt32(Console.ReadLine());
+int gros=a;
+if (gros <= b)
+{
+    Console.WriteLine($"Промежуточный максимум  {b}");
+}
+if (b<= c3)
+{
+     Console.WriteLine($"Большее число  {c3}");
+}
+
+
+
