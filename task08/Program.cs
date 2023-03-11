267c0b112b102ba4f8ae2cb8032037ac7eeb8905
TatNik Mosku <lusmo1@yandex.ru>
2023-03-11 12:15:44 +0700

    Программа 8 готова task08

---
+++ task08/Program.cs
@@ -0,0 +1,16 @@
+﻿// 1. Ввети число
+// 2. Вывести все четные числа, включая само число
+// 
+Console.WriteLine("Введите целое положительное число ");
+int hi=Convert.ToInt32(Console.ReadLine());
+int count=-hi;
+Console.WriteLine("Четные числа, входящие в диапазон -hi до hi: ");
+while (count<=hi)
+{
+   if (count%2==0)
+   {
+        Console.Write($"   {count}"); 
+   }
+   count++;
+}
+
