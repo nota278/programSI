8548802e050be6c6bc2c84f96c544d82e3f026c4
TatNik Mosku <lusmo1@yandex.ru>
2023-03-11 09:42:55 +0700

    Программа 3 отлажена task03

---
+++ task03/Program.cs
@@ -0,0 +1,43 @@
+﻿// 1. Задается номер дня недели
+// 2. На выходе выводится текстом день недели
+// В программе предусмотрена проверка на дуракоустойчивость.
+// То есть, если номер дня недели
+// выходит за рамки от 1 до 7
+Console.WriteLine("Введите номер дня недели");
+int a =Convert.ToInt32(Console.ReadLine());
+if (a< 1) 
+{  
+     Console.WriteLine("   Введите правильно номер дня недели от 1 до 7");
+}
+
+if (a>7)
+{
+    Console.WriteLine("   Введите правильно номер дня недели от 1 до 7");
+}
+else if (a==1)
+{
+     Console.WriteLine("   Это понедельник");
+}
+else if(a==2)
+{
+    Console.WriteLine("   Это вторник");
+}
+else if (a==3)
+{
+    Console.WriteLine("   Это среда");
+}
+else if (a==4)
+{
+    Console.WriteLine("   Это четверг");
+}
+else if (a==5)
+{
+    Console.WriteLine("   Это пятница");    
+}
+else if (a==6)
+{
+    Console.WriteLine("   Это суббота");
+}
+else if (a==7)
+    Console.WriteLine("   Это воскресенье");
+
