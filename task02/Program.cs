1f2d9631fd13cc84e7ccf5f72664281b0eb09b2d
TatNik Mosku <lusmo1@yandex.ru>
2023-03-11 07:54:11 +0700

    Отладила 2 прогу task02

---
+++ task02/Program.cs
@@ -0,0 +1,21 @@
+﻿// 1.На вход подаются два числа
+// 2.Вывести максимальное из них
+
+Console.WriteLine("Введите первое число");
+int a=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b=Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine($"Большее число   {a}");
+}
+else if (b > a)
+{
+    Console.WriteLine($"Большее число   {b}");
+}
+else if (a==b)
+{
+    Console.WriteLine($"Оба числа равны   {a = b}");
+}
+
