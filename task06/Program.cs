03b7bfa7bbfc14caa3a37175426ee8cd483f837f
TatNik Mosku <lusmo1@yandex.ru>
2023-03-11 11:22:16 +0700

    Программа 6 готова task06

---
+++ task06/Program.cs
@@ -0,0 +1,15 @@
+﻿// 
+// 
+// 
+// 
+Console.WriteLine("Введите целое  число число");
+int hi=Convert.ToInt32(Console.ReadLine());
+if (hi%2 == 0)
+{
+    Console.WriteLine("  Число четное");
+}
+else
+{
+    Console.WriteLine("  Число не четное");
+}
+
