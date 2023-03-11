5d9a994933c9b1e034cf1e2d8331a6339dde653f
TatNik Mosku <lusmo1@yandex.ru>
2023-03-11 10:59:20 +0700

    Программа 5 вывод в строку task05

---
+++ task05/Program.cs
@@ -0,0 +1,12 @@
+﻿// 1. Вводим целое натуральное число
+// 2. Получить ряд чисел от -n до n
+// 
+
+Console.WriteLine("Введите целое положительное число число");
+int a=Convert.ToInt32(Console.ReadLine());
+int count = -a;
+while (count <= a)
+{
+    Console.Write($"   {count}");
+    count++;
+}
