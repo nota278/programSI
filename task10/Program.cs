46e433660a3b032660b57f812ad973593e6060f9
TatNik Mosku <lusmo1@yandex.ru>
2023-03-12 07:45:44 +0700

    Программа 10 task10

---
+++ task10/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное 
+// число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+Console.WriteLine("Введите целое положительное 3-х значное число ");
+int hi=Convert.ToInt32(Console.ReadLine());
+int result = Twochi(hi);
+Console.WriteLine($"Вторая цифра числа {hi} = {result}");
+int Twochi(int num)
+{
+    int zifra1 = num/10;
+    int zifra2 = zifra1%10;
+    return zifra2;
+    
+}
