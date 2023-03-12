0019db20f4a7ff0e00c81a6739ef962dba05b4d8
TatNik Mosku <lusmo1@yandex.ru>
2023-03-12 09:20:51 +0700

    Программа 12 task12

---
+++ task12/Program.cs
@@ -0,0 +1,21 @@
+﻿// 1. На вход подаются два числа
+// 2. Является ли первое число
+// кратным второму. Если первое не кратно второму,
+// то выводится остаток от деления
+//  34, 5   не кратно, остаток 4
+//  16, 4   кратно
+Console.WriteLine("Введите первое число");
+int a=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b=Convert.ToInt32(Console.ReadLine());
+int result = Multipli(a,b);
+if (result==0 )
+    Console.WriteLine("Число кратно");
+else
+    Console.WriteLine($"Остаток от деления  {result}");
+int Multipli(int num1, int num2)
+{
+    int zifra1 = num1%num2;
+    
+    return zifra1;
+}    
