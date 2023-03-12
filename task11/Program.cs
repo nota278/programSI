150240c7d0131dfc416baa0d0b0a290faf6925c1
TatNik Mosku <lusmo1@yandex.ru>
2023-03-12 08:10:43 +0700

    Программа 11  task11

---
+++ task11/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 11: Напишите программу, которая принимает на вход трёхзначное 
+// число и удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+Console.WriteLine("Введите целое положительное 3-х значное число ");
+int hi=Convert.ToInt32(Console.ReadLine());
+int result = Twochi(hi);
+Console.WriteLine($"Из  {hi}  получили  {result}");
+int Twochi(int num)
+{
+    int zifra1 = num/100;
+    int zifra3 = num%10;
+    int zifra2 = zifra1%10;
+    return (zifra1*10+zifra3);
+    
+}
