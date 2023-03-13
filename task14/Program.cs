d19c8fce71acd9f5f32a1620bac53dc5db28fee0
TatNik Mosku <lusmo1@yandex.ru>
2023-03-13 19:54:18 +0700

    Программа 14  task14

---
+++ task14/Program.cs
@@ -0,0 +1,14 @@
+﻿// 1. Ввести число
+// 2. Проверить, кратно ли оно одновременно
+// 7 и 23
+//   14 - нет
+//   46 - нет
+//   161- да
+Console.WriteLine("Введите целое число ");
+int hi=Convert.ToInt32(Console.ReadLine());
+bool result = Multipl(hi,7,23);
+Console.WriteLine(result);
+bool Multipl (int num, int num1, int num2)
+{
+    return (num%num1==0 && num%num2==0);
+}
