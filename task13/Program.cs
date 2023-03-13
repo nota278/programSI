6a24336ba11136917fa9aad9d7ab08855853b157
TatNik Mosku <lusmo1@yandex.ru>
2023-03-13 19:12:41 +0700

    Программа 13 со строками  task13

---
+++ task13/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 13: Напишите программу, которая выводит 
+// третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+string userString="";
+Console.WriteLine("Введите    целое положительное    число ");
+userString = Console.ReadLine();
+int dl=userString.Length;
+if (dl<=2)  
+    Console.WriteLine("Третьей цифры нет "); 
+Console.WriteLine($"длина числа  {dl} ");
+Console.WriteLine($"Третья цифра= {userString[2]} ");
+
