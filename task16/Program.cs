21d5d20a6e4e3036b974fc938ba69dffd020140f
TatNik Mosku <lusmo1@yandex.ru>
2023-03-13 22:36:18 +0700

    Программа 16 task16

---
+++ task16/Program.cs
@@ -0,0 +1,14 @@
+﻿//1. На вход подаются два числа 
+//2. Является ли одно число 
+// квадратом другого
+Console.WriteLine("Введите первое число ");
+int first=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int second=Convert.ToInt32(Console.ReadLine());
+bool result= Squart(first, second);
+Console.WriteLine(result ? "да" : "нет");
+bool Squart(int num1, int num2)
+{
+    return (num1*num1==num2 ||num2*num2==num1);
+
+}
