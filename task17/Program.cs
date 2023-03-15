f5f5c60c4b7bb06f6238ebf5a94593e2819345b5
TatNik Mosku <lusmo1@yandex.ru>
2023-03-15 22:39:27 +0700

    Программа 17  task17

---
+++ task17/Program.cs
@@ -0,0 +1,22 @@
+﻿//  По введенным координатам точки
+//  определить в каком квадранте
+//  она находится
+Console.WriteLine("Введите ординату Х ");
+Console.WriteLine("X:   ");
+int xordin=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите ординату Y ");
+Console.WriteLine("Y:   ");
+int yordin=Convert.ToInt32(Console.ReadLine());
+int qwad=Quart(xordin, yordin);
+string result = qwad >0 
+             ? $"Указанные координаты соответствуют четверти  {qwad} "
+             : "Вы ввели некорректные ординаты";
+Console.WriteLine(result);            
+int Quart(int x, int y)
+{
+   if (x>0 && y>0) return 1;
+   if (x<0 && y>0) return 2;
+   if (x<0 && y<0) return 3;
+   if (x>0 && y<0) return 4;
+   return 0;
+}
