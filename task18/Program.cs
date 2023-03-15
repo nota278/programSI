91db1b74d9ebdd5ab934cb0322cec5d3ad742775
TatNik Mosku <lusmo1@yandex.ru>
2023-03-15 23:16:27 +0700

    Программа 18 task18

---
+++ task18/Program.cs
@@ -0,0 +1,18 @@
+﻿// По номеру четверти
+// показать диапазон изменения
+// координат точки (x,y)
+Console.WriteLine("Введите номер четверти");
+int chet=Convert.ToInt32(Console.ReadLine());
+string result= Koord (chet);
+Console.WriteLine(result);
+
+string Koord (int ch)
+{
+    if (ch==1) return "x > 0, y > 0";
+    if (ch==2) return "x < 0, y > 0";
+    if (ch==3) return "x < 0, y < 0";
+    if (ch==4) return "x > 0, y < 0";
+    return "Введена некорректная четверть";
+
+}    
+
