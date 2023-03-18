927f8e5995bed99fdee67bec8e0b77cccb733f60
TatNik Mosku <lusmo1@yandex.ru>
2023-03-18 07:41:10 +0700

    Программа 22 task22

---
+++ task22/Program.cs
@@ -0,0 +1,18 @@
+﻿// Зад. 22: на вход подается число N
+// На выходе - таблица квадратов 
+// чисел от 1 до N
+
+Console.WriteLine("Введите натуральное число");
+int chi=Convert.ToInt32(Console.ReadLine());
+TablSqrt (chi);
+
+void TablSqrt (int num)
+{  
+    int count=1;
+    while (count <= num)
+    {
+       int sq = count*count;
+       Console.WriteLine($"{count, 3} ->  {sq}");
+        count++;
+    }
+}
