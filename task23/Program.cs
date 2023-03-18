a0af9b9d999e59895535b2d91f526ca53827035d
TatNik Mosku <lusmo1@yandex.ru>
2023-03-18 07:50:56 +0700

    Программа 23 task23

---
+++ task23/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает
+//  на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("Введите натуральное число");
+int chi=Convert.ToInt32(Console.ReadLine());
+TablSkub (chi);
+
+void TablSkub (int num)
+{  
+    int count=1;
+    while (count <= num)
+    {
+       int sq = count*count*count;
+       Console.WriteLine($"{count, 3} ->  {sq}");
+        count++;
+    }
+}
+
+
+
+
+
+
+
+
