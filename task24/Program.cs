0f7cbf0382103c07d8e53834c51ba96b6d1b18b9
TatNik Mosku <lusmo1@yandex.ru>
2023-03-19 05:51:21 +0700

    Программа 24 task24

---
+++ task24/Program.cs
@@ -0,0 +1,24 @@
+﻿// На вход подается число А
+// на выходе получить сумму
+// чисел от 1 до А
+
+Console.WriteLine("Введите натуральное число");
+int chi=Convert.ToInt32(Console.ReadLine());
+int sumnum= SumNumbers(chi);
+Console.WriteLine($"Сумма чисел от А до {chi} = {sumnum}");
+
+int SumNumbers(int num)
+{  
+    
+    int sum = 0;
+    for (int i = 1; i <= num; i++) 
+    {
+        sum += i;
+
+    }
+
+        return sum;  
+
+
+
+}       
