fd09dfc036c130c619b5de8178d51ae22842a3bc
TatNik Mosku <lusmo1@yandex.ru>
2023-03-19 13:28:23 +0700

    Программа 30  task30

---
+++ task30/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 30: Напишите программу, которая задаёт 
+// массив из 8 элементов, заполненный нулями и единицами
+// в случайном порядке
+
+int [] array = new int [8];
+FillArray (array);
+PrintArray (array);
+void FillArray (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array [i] = new Random().Next (0,2);
+
+    }
+
+}
+void PrintArray (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)    
+    {
+        Console.Write (array[i] + "  ");
+    }
+}
