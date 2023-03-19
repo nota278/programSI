f16e517d6900cb1bd9989ffc9309e7ea26a6c490
TatNik Mosku <lusmo1@yandex.ru>
2023-03-19 09:42:52 +0700

    Программа 28  task28

---
+++ task28/Program.cs
@@ -0,0 +1,17 @@
+﻿// Ввести число N и подсчитать
+// произведение чисел от 1 до N 
+
+Console.WriteLine("Введите натуральное число ");
+int chi = Convert.ToInt32(Console.ReadLine());
+int result = Composition (chi);
+Console.WriteLine($"Произведение цифр числа  {chi} = {result} ");
+int Composition (int num)
+{
+    int p = 1;
+    
+    for (int i = 2; i <=num; i++)
+    {
+        p = p * i;
+    }
+    return p;
+}
