d7c7959f61e7e243d2b530b9a42ce48d3f6b2c67
TatNik Mosku <lusmo1@yandex.ru>
2023-03-19 09:17:45 +0700

    Программа 27  task27

---
+++ task27/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 27: Напишите программу, которая принимает
+// на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите натуральное число ");
+int chi = Convert.ToInt32(Console.ReadLine());
+int result = Sumzi (chi);
+Console.WriteLine($"Сумма цифр числа  {chi} = {result} ");
+int Sumzi (int num)
+{
+    int sum = 0;
+    
+    while (num > 0)
+    {           
+        sum = sum + num % 10;
+         num  /= 10;
+    }
+
+    return sum;
+}
