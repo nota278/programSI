9befdd9874eaebd97d97903d93b40443087f00a8
TatNik Mosku <lusmo1@yandex.ru>
2023-03-19 07:48:33 +0700

    Программа 26  task26

---
+++ task26/Program.cs
@@ -0,0 +1,18 @@
+﻿// Ввести число и подсчитать
+// количество цифр числа
+Console.WriteLine("Введите натуральное число ");
+int chi = Convert.ToInt32(Console.ReadLine());
+int result = Kolzi (chi);
+Console.WriteLine($"Количество цифр числа  {chi} = {result} ");
+int Kolzi (int num)
+{
+    int count = 0;
+    while (num > 0)
+    {   
+        count++;
+        num  /= 10; 
+        
+    }
+
+    return count;
+}
