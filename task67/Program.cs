c6c34e07049ea6be0cc8e984a87dc629906e8292
TatNik Mosku <lusmo1@yandex.ru>
2023-04-06 14:09:38 +0700

    Программа67  task67

---
+++ task67/Program.cs
@@ -0,0 +1,16 @@
+﻿// Зад.67. Программа принимает на вход число
+// и возващает сумму его цифр.
+// 453 -> 12
+// 45 - > 9
+
+Console.WriteLine ("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = SumDigits (number);
+Console.WriteLine ($"Сумма цифр числа {number} -> {sum}");
+int SumDigits (int num)
+{
+    if (num == 0) return 0;
+    else return SumDigits (num/10) + num%10;
+ 
+
+}
